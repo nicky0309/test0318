150ce26285eae9fd2a3101254187a76991a52973
nicky0309 <80886742+nicky0309@users.noreply.github.com>
2021-03-28 18:18:24 +0900

    Add files via upload

---
+++ Indexof.java
@@ -0,0 +1,24 @@
+package LIFO_FIFO;
+
+import java.io.File;
+
+public class Indexof {
+
+	public static void main(String[] args) {
+		/*
+		String fileAddr = "C:\\FXProject\\Bannana\\star.png";
+		String arriveFileAddr = "C:\\\\FXProject\\\\Bannana\\\\star.png";				
+		int a = fileAddr.lastIndexOf("\\");
+		File file = new File(fileAddr.substring(a));
+		System.out.println(fileAddr.substring(a));
+		*/
+		
+		String tmpAddr = "C:\\FXProject\\tmpmemory";
+		String newAddr = "C:\\FXProject\\Bannana";
+		
+		System.out.println("C:\\FXProject\\Apple\\star.png");
+	
+		
+	}
+
+}
